222d69932cd969aa056cfebdb93959582bf3cc0a
tylerdgenius <albertnwachukwu@gmail.com>
2022-07-10 17:56:49 +0100

    Upgraded Version

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,45 +1,53 @@
-import { normalCaseGenerator } from '../'
+import { normalCaseGenerator } from '../';
 
 /**
  * Testing the capitalize function without options
  */
 test('Testing Capitalize', () => {
-    expect(normalCaseGenerator('this is a test')).toBe('This is a test')
+  expect(normalCaseGenerator('this is a test')).toBe('This is a test');
 });
 
 /**
  * Testing the capitalize function with the option of 'all'
  */
 test('Testing Capitalize All Text', () => {
-    expect(normalCaseGenerator('this is a test', {
-        filter: 'all',
-    })).toBe('This Is A Test')
+  expect(
+    normalCaseGenerator('this is a test', {
+      filter: 'all',
+    }),
+  ).toBe('This Is A Test');
 });
 
 /**
  * Testing the capitalize function with the option of 'first'
  */
 test('Testing Capitalize First Text', () => {
-    expect(normalCaseGenerator('this is a test', {
-        filter: 'first',
-    })).toBe('This is a test')
+  expect(
+    normalCaseGenerator('this is a test', {
+      filter: 'first',
+    }),
+  ).toBe('This is a test');
 });
 
 /**
  * Testing the capitalize function to capitalize a single text
  */
 test('Testing Capitalize Single Text', () => {
-    expect(normalCaseGenerator('this is a test', {
-        filter: 'single',
-        textToCapitalize: 'test'
-    })).toBe('this is a Test')
+  expect(
+    normalCaseGenerator('this is a test', {
+      filter: 'single',
+      textToCapitalize: 'test',
+    }),
+  ).toBe('this is a Test');
 });
 
 /**
  * Testing the capitalize function to capitalize a single text without passing the textToFind
  */
- test('Testing Capitalize Single Text', () => {
-    expect(normalCaseGenerator('this is a test', {
-        filter: 'single',
-    })).toBe('this is a test')
+test('Testing Capitalize Single Text', () => {
+  expect(
+    normalCaseGenerator('this is a test', {
+      filter: 'single',
+    }),
+  ).toBe('this is a test');
 });
